7fb0a358b5e57afbc5ba2ac1d1147f7d0a7dd523
Ben <abrahamyan.ruben@gmail.com>
2017-12-02 03:02:59 +0400

    fixes

---
+++ NorTari/Assets/Scripts/FPS.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+
+    private float deltaTime = 0.0f;
+    private Text textLabel;
+
+    void Awake () {
+        textLabel = transform.GetComponent<Text>();
+    }
+
+    void Update () {
+        deltaTime += ( Time.deltaTime - deltaTime ) * 0.1f;
+    }
+
+    void OnGUI () {
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string fpsText = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        textLabel.text = fpsText;
+    }
+}
